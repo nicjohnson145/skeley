05b812665fd2a9f7c67379ae8fbbe0ccc183cb07
Nic Johnson <nicjohnson145@hotmail.com>
2023-01-27 20:12:45 -0600

    fix: beef up the logging and config handling bits

--- main.go
+++ main.go
@@ -10,10 +10,7 @@ import (
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
-)
-
-const (
-	TemplateDir = "/home/njohnson/scratch/skeley-templates"
+	"github.com/rs/zerolog/log"
 )
 
 type TemplateConfig struct {
@@ -28,36 +25,50 @@ type TemplateVars struct {
 }
 
 func root() *cobra.Command {
-	return &cobra.Command{
-		Use: "skeley",
+	var templateDir string
+	var debug bool
+
+	root := &cobra.Command{
+		Use: "skeley [OPTS] <template-name>",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			initializeConfig(cmd)
 			cmd.SilenceUsage = true
 		},
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			templateDir := filepath.Join(TemplateDir, args[0])
+			initLogging(debug)
+			if templateDir == "" {
+				home, err := os.UserHomeDir()
+				if err != nil {
+					log.Err(err).Msg("getting home directory")
+					return fmt.Errorf("error getting user home directory: %w", err)
+				}
+				templateDir = filepath.Join(home, ".config", "skeley", "templates")
+			}
 
-			confBytes, err := os.ReadFile(filepath.Join(templateDir, "config.yaml"))
+			selectedTemplate := filepath.Join(templateDir, args[0])
+
+			confBytes, err := os.ReadFile(filepath.Join(selectedTemplate, "config.yaml"))
 			if err != nil {
-				fmt.Println("err reading config")
+				log.Err(err).Msg("reading skeley config")
 				return err
 			}
 
 			var conf TemplateConfig
 			if err := yaml.Unmarshal(confBytes, &conf); err != nil {
-				fmt.Println("err unmarshalling")
+				log.Err(err).Msg("unmarshalling skeley config")
 				return err
 			}
 
 			module, err := getModule()
 			if err != nil {
-				fmt.Println("err getting module")
+				log.Err(err).Msg("reading go module information")
 				return err
 			}
 
 			root, err := findAndParseTemplates(filepath.Join(templateDir, "files"), template.FuncMap{})
 			if err != nil {
-				fmt.Println("err parsing templates")
+				log.Err(err).Msg("parsing template files")
 				return err
 			}
 
@@ -67,7 +78,7 @@ func root() *cobra.Command {
 			}
 			for _, fl := range conf.Files {
 				if err := renderFile(root, fl, tmplVars); err != nil {
-					fmt.Println("err rendering templates")
+					log.Err(err).Msg("rendering template files")
 					return err
 				}
 			}
@@ -75,20 +86,28 @@ func root() *cobra.Command {
 			return nil
 		},
 	}
+
+	root.Flags().StringVarP(&templateDir, "template-dir", "t", "", "Override the default template directory")
+	root.Flags().BoolVarP(&debug, "debug", "d", false, "Enable debug logging")
+
+	return root
 }
 
 func renderFile(tmpl *template.Template, name string, vars TemplateVars) error {
 	if err := os.MkdirAll(filepath.Dir(name), 0775); err != nil {
+		log.Err(err).Msg("making containing directory")
 		return err
 	}
 
 	f, err := os.OpenFile(name, os.O_RDWR | os.O_CREATE | os.O_TRUNC, 0664)
 	if err != nil {
+		log.Err(err).Msg("opening target file")
 		return err
 	}
 	defer f.Close()
 
 	if err := tmpl.ExecuteTemplate(f, name, vars); err != nil {
+		log.Err(err).Msg("executing template")
 		return err
 	}
 
@@ -98,6 +117,7 @@ func renderFile(tmpl *template.Template, name string, vars TemplateVars) error {
 func getModule() (string, error) {
 	modBytes, err := os.ReadFile("go.mod")
 	if err != nil {
+		log.Err(err).Msg("reading go.mod")
 		return "", err
 	}
 
@@ -118,6 +138,7 @@ func findAndParseTemplates(rootDir string, funcMap template.FuncMap) (*template.
 
 			b, e2 := os.ReadFile(path)
 			if e2 != nil {
+				log.Err(e2).Str("path", path).Msg("reading template file")
 				return e2
 			}
 
@@ -125,7 +146,7 @@ func findAndParseTemplates(rootDir string, funcMap template.FuncMap) (*template.
 			t := root.New(name).Funcs(funcMap)
 			_, e2 = t.Parse(string(b))
 			if e2 != nil {
-				fmt.Printf("err parsing template %v\n", path)
+				log.Err(e2).Str("path", path).Msg("parsing template file")
 				return e2
 			}
 		}
