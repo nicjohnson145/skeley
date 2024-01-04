11ebdf91ca8e208f8a054e646848a9ec727a5652
Nic Johnson <nicjohnson145@hotmail.com>
2024-01-04 12:15:47 -0600

    fix: correct list command

--- cmd/list.go
+++ cmd/list.go
@@ -2,28 +2,25 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/nicjohnson145/skeley/config"
 	"github.com/nicjohnson145/skeley/internal"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func List() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "list",
-		Short: "List available templates",
+		Short: "List available local templates",
 		Args: cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			log := config.InitLogger()
 
-			inputFS, err := internal.InputFSFromEnv(log, args[0])
-			if err != nil {
-				return err
-			}
-
 			skeley := internal.NewSkeley(internal.SkeleyConfig{
 				Logger: log,
-				InputFS: inputFS,
+				InputFS: os.DirFS(viper.GetString(config.TemplateDir)),
 			})
 
 			tmpls, err := skeley.ListTemplates()
